50b3fa78f77c02c304fcc13d784dde130af0bfb9
Abhinashredddy <133764448+Abhinashredddy@users.noreply.github.com>
2024-01-14 08:14:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
-https://github.com/Abhinashredddy/adv_alg_week_1.git
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> num_map;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (num_map.find(complement) != num_map.end()) {
+            return { num_map[complement], i };
+        }
+        num_map[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = twoSum(nums, target);
+    if (!result.empty()) {
+        std::cout << "Indices: " << result[0] << ", " << result[1] << std::endl;
+    } else {
+        std::cout << "No solution found." << std::endl;
+    }
+    return 0;
+}
